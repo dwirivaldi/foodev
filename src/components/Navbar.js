8ad685ac69732e84439ea304ba4702c4a4343552
dwirivaldi <dwirivaldi.i@gmail.com>
2022-12-21 22:57:34 +0700

    sidebar icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 export default function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div className="navbar container">
       <a href="#!" className="logo">
         F<span style={{ color: "black" }}>oo</span>diesDev
       </a>
       <div className="nav-links">
-        <a href="#!" className="active">
+        <a href="#!" className="">
           Home
         </a>
         <a href="#!">Recipes</a>
         <a href="#!">Setting</a>
+      </div>
+      <div
+        onClick={() => setShowSidebar(!showSidebar)}
+        className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </div>
   );
